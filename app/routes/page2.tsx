f5954a0727716ec8fa70fa10fa923a88778f22c7
Caleb Melnychenko <calebinski98@gmail.com>
2025-08-02 23:15:56 -0400

    Added a page 2

---
+++ app/routes/page2.tsx
@@ -0,0 +1,7 @@
+export default function Page2() {
+    return (
+    <div className="page2-Content">
+        <h1 className="page2-content-text">This is where the Page2 page content goes!</h1>
+    </div>
+    );
+}
