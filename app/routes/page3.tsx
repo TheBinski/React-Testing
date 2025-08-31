a9683fde1ca5827af39507f899b46c4877ae1eec
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-08-31 19:34:50 -0400

    Mariah - finished number game on page 3

--- app/routes/page3.tsx
+++ app/routes/page3.tsx
@@ -1,33 +1,55 @@
+import "../styles/page3.css";
 import { useState } from "react";
 
 export default function Page3(){
     const [number, setNumber] = useState(Math.floor(Math.random() * (100 - 1 + 1)) + 1);
     const [inputNum, setInputNum] = useState(0);
+    const [headingMessage, setHeadingMessage] = useState<string>("Guess the number below!");
+    const [resetButton, setResetButton] = useState(false);
+    
 
     function updateInputNum(event: any){
         setInputNum(event.target.value);
     }
 
+    function updateNumber(){
+        setNumber(Math.floor(Math.random() * (100 - 1 + 1)) + 1);
+        setResetButton(false);
+        setHeadingMessage("Guess the number below!");
+        setInputNum(0);
+    }
+
     
     function numCompare() {
-        console.log("input: ", inputNum);
-        console.log("num: ", number);
-
         if(inputNum == number){
-            console.log("Congratulations! You guessed correctly!");
+            setHeadingMessage("Congratulations! You guessed correctly!");
+            setResetButton(true);
         }
-        else {
-            console.log("Sorryl that is incorrect. Please guess again.");
+        else if (inputNum > number){
+            setHeadingMessage("Sorry; that is too High. Guess again");
+        }
+        else if(inputNum < number){
+            setHeadingMessage("Sorry; that is too low. Guess again");
         }
         
     }
 
     return (
-        <div>
-            <h1>{number}</h1>
+        <div className="pageDiv">
             <div>
-                <input type="number" value={inputNum} onChange={updateInputNum}></input>
-                <button onClick={numCompare}>Enter Guess</button>
+                <h1 style={{fontSize: "40px"}}>Number Guessing Game</h1>
+                <br>
+                </br>
+                <h2>The computer has generated a random number between 1 and 100. It is your job to guess the number.</h2>
+                <br>
+                </br>
+                {/* <h1>{number}</h1> */}
+                <h1>{headingMessage}</h1>
+                <div>
+                    <input type="number" value={inputNum} onChange={updateInputNum}></input>
+                    <button className="actionButton submitButton" onClick={numCompare}>Enter Guess</button>
+                    <button className="actionButton resetButton" onClick={updateNumber} disabled={!resetButton}>Reset</button>
+                </div>
             </div>
         </div>
     );
